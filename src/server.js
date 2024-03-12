7283e2c33ac76bcbb44fe4e2af80b9b986b4944f
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-03-12 20:34:00 +0700

    learn to use env to config constant

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-const port = 8080
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3030;//8080
+const hostname = process.env.HOSTNAME; //localhost
 
 //config template 
 app.set('views', path.join(__dirname, 'views'))
@@ -13,6 +16,6 @@ app.get('/', (req, res) => {
 app.get('/dumb', (req, res) => {
   res.render('sample.ejs')
 })
-app.listen(port, () => {
+app.listen(port,hostname, () => {
   console.log(`Example app listening on port ${port}`)
 })
