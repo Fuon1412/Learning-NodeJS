038a327d17b3fc5f4bdf23f6f723dd9613c341c3
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-03-19 22:11:04 +0700

    learn to connect database

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
-
 const webRoutes = require('./routes/web');
+
+const mysql = require('mysql2');
 const configViewEngine = require('./config/viewEngine');
 const app = express();
 const port = process.env.PORT || 3030;//8080
 const hostname = process.env.HOSTNAME; //localhost
 
 configViewEngine(app);
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  password: '123456',
+  database: 'hoidanit',
+});
+
+connection.query(
+      'select * from Users u',
+      function (err,results, fields){
+        console.log(results);
+        console.log(fields);
+      }
+);
 
 app.use('/', webRoutes);
 app.listen(port,hostname, () => {
