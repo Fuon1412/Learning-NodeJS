14415c9012950923d81a6dfafc170b7e6ba31f15
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-03-21 12:52:08 +0700

    config database

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 require('dotenv').config();
 const webRoutes = require('./routes/web');
 
-const mysql = require('mysql2');
+const connection = require('./config/database');
 const configViewEngine = require('./config/viewEngine');
 const app = express();
 const port = process.env.PORT || 3030;//8080
@@ -11,19 +11,11 @@ const hostname = process.env.HOSTNAME; //localhost
 
 configViewEngine(app);
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '123456',
-  database: 'hoidanit',
-});
 
 connection.query(
       'select * from Users u',
       function (err,results, fields){
         console.log(results);
-        console.log(fields);
       }
 );
 
