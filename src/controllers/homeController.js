fee406a9157332c4a91fcff8ef005ca9cba77527
Phuong Do Manh <phuong.dm225660@sis.hust.edu.vn>
2024-03-26 23:38:18 +0700

    learn to use async/await

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,30 +1,28 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-   return res.render("home.ejs");
+  return res.render("home.ejs");
+};
+const getCreatePage = (req, res) => {
+  return res.render("create.ejs");
 };
 
 const getDumbPage = (req, res) => {
   res.render("sample.ejs");
 };
 
-const postCreateUser = (req, res) => {
-  let { email, name, city } = req.body;
-  connection.query(
-    `INSERT INTO 
-    Users (email, name, city) 
-    VALUES (?, ?, ?)`,
-    [email, name, city],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      }
-    }
-  )
-  res.send('User created');
+const postCreateUser = async (req, res) => {
+  let { email, myName: name, city } = req.body;
+  console.log(email, name, city);
+  let [results, fields] = await connection.query(
+    `INSERT INTO Users (email, name, city) VALUES (?, ?, ?)`,
+    [email, name, city]
+  );
+  res.send("User created");
 };
 module.exports = {
   getHomePage,
   getDumbPage,
-  postCreateUser
+  postCreateUser,
+  getCreatePage,
 };
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,8 +1,15 @@
-const { getHomePage , getDumbPage, postCreateUser } = require('../controllers/homeController');
+const {
+  getHomePage,
+  getDumbPage,
+  postCreateUser,
+  getCreatePage,
+} = require("../controllers/homeController");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", getHomePage);
 router.get("/dumb", getDumbPage);
-router.post('/create-user', postCreateUser);
+router.get("/create", getCreatePage);
+router.post("/create-user", postCreateUser);
+
 module.exports = router;
